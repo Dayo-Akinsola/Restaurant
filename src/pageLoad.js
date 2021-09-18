538221cfdf13c154aca5bc91bd650f4e82d866b4
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-09-18 20:16:21 +0100

    Created functions to render the about section on the homepage

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,4 +1,7 @@
-const createHeader = () => {
+
+const dummyText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
+const createHeader = (header) => {
     const body = document.querySelector('body');
     const headerDiv = document.createElement('div');
     headerDiv.classList.add('header');
@@ -31,7 +34,8 @@ const createHeader = () => {
 
     headerDiv.appendChild(title);
     headerDiv.appendChild(navDiv);
-    body.appendChild(headerDiv);
+    header.appendChild(headerDiv)
+    body.appendChild(header);
 }
 
 // Helper function for createContent to set text, id and class for an element
@@ -47,10 +51,10 @@ const renderContent = (containerID, textID, info, tag) => {
     return div;
 }
 
-const createContent = () => {
+// Renders the top part of the home page when the page is loaded.
+const createContent = (header) => {
     const body = document.querySelector('body');
     const contentContainer = document.createElement('div');
-    const headlineDiv = document.createElement('div');
     const aboutButton = document.createElement('button');
 
     contentContainer.setAttribute('id', 'content-container');
@@ -61,9 +65,58 @@ const createContent = () => {
 
     const elementArray = [welcome, headlineDiv, aboutButton];
     elementArray.forEach(element => contentContainer.appendChild(element));
+    header.appendChild(contentContainer);
     
-    body.appendChild(contentContainer);
+    body.appendChild(header);
+
+}
+
+// Helper function to add images to the dom
+const renderImage = (id, source) => {
+    const imageDiv = document.createElement('div');
+    const image = document.createElement('img');
+
+    imageDiv.setAttribute('id', id);
+    imageDiv.classList.add('image-container');
+    image.src = source;
+    imageDiv.appendChild(image);
+
+    return imageDiv;
+}
+
+const renderTextSection = (text, subHeadingText) => {
+    const textDiv = document.createElement('div');
+    const textSubHeading = document.createElement('h3');
+    const mainText = document.createElement('p');
+
+    textDiv.classList.add('text-container');
+    textSubHeading.classList.add('subheading');
+    textSubHeading.textContent = subHeadingText;
+    mainText.classList.add('main-text');
+    mainText.textContent = text;
+
+    textDiv.appendChild(textSubHeading); 
+    textDiv.appendChild(mainText);
+    return textDiv;
+}
+
+const aboutSection = () => {
+    const body = document.querySelector('body');
+    const aboutSection = document.createElement('div');
+
+    aboutSection.setAttribute('id', 'about-section');
+
+    const imageOneDiv = renderImage('imageOne-container', 'https://nomadparadise.com/wp-content/uploads/2020/08/nigerian-food-09.jpg');
+    const textDiv = renderTextSection(dummyText, 'About Us');
+    const imageTwoDiv = renderImage('imageTwoDiv', 'https://nomadparadise.com/wp-content/uploads/2020/08/nigerian-food-13.jpg');
+
+    const elementArray = [imageOneDiv, textDiv, imageTwoDiv];
+
+    elementArray.forEach(element => aboutSection.appendChild(element));
+
+    body.appendChild(aboutSection);
+
 
 }
  
-export { createHeader, createContent};
+export { createHeader, createContent, aboutSection};    
