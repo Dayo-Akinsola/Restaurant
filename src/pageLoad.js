d3cace6964056cc318b3ce857e9fe53dca62fc4b
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-09-18 22:39:58 +0100

    Removed helper functions and stored them in thier own module

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,3 +1,4 @@
+import { renderContent, renderTextSection, renderImage } from "./helpers";
 
 const dummyText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
@@ -38,18 +39,6 @@ const createHeader = (header) => {
     body.appendChild(header);
 }
 
-// Helper function for createContent to set text, id and class for an element
-const renderContent = (containerID, textID, info, tag) => {
-    const div = document.createElement('div');
-    const text = document.createElement(tag);
-
-    div.classList.add('content'); div.setAttribute('id', containerID);
-    text.setAttribute('id', textID);
-    text.innerHTML = info;
-    div.appendChild(text);
-
-    return div;
-}
 
 // Renders the top part of the home page when the page is loaded.
 const createContent = (header) => {
@@ -71,35 +60,6 @@ const createContent = (header) => {
 
 }
 
-// Helper function to add images to the dom
-const renderImage = (id, source) => {
-    const imageDiv = document.createElement('div');
-    const image = document.createElement('img');
-
-    imageDiv.setAttribute('id', id);
-    imageDiv.classList.add('image-container');
-    image.src = source;
-    imageDiv.appendChild(image);
-
-    return imageDiv;
-}
-
-const renderTextSection = (text, subHeadingText) => {
-    const textDiv = document.createElement('div');
-    const textSubHeading = document.createElement('h3');
-    const mainText = document.createElement('p');
-
-    textDiv.classList.add('text-container');
-    textSubHeading.classList.add('subheading');
-    textSubHeading.textContent = subHeadingText;
-    mainText.classList.add('main-text');
-    mainText.textContent = text;
-
-    textDiv.appendChild(textSubHeading); 
-    textDiv.appendChild(mainText);
-    return textDiv;
-}
-
 const aboutSection = () => {
     const body = document.querySelector('body');
     const aboutSection = document.createElement('div');
@@ -111,7 +71,6 @@ const aboutSection = () => {
     const imageTwoDiv = renderImage('imageTwoDiv', 'https://nomadparadise.com/wp-content/uploads/2020/08/nigerian-food-13.jpg');
 
     const elementArray = [imageOneDiv, textDiv, imageTwoDiv];
-
     elementArray.forEach(element => aboutSection.appendChild(element));
 
     body.appendChild(aboutSection);
