e5579396e5ad9725732d0e703412e771b9e9bcbd
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-09-20 12:06:06 +0100

    Refactored functions for about and welcome sections

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,63 +1,28 @@
 import { renderContent, renderTextSection, renderImage } from "./helpers";
+import { createHeader } from "./index.js";
 
 const dummyText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
-const createHeader = (headerTag) => {
-    const body = document.querySelector('body');
-    const headerDiv = document.createElement('div');
-    headerDiv.classList.add('header');
-
-    const title = document.createElement('h3');
-    title.setAttribute('id', 'title');
-    title.innerHTML = 'Otit&#7885';
-
-    const navDiv = document.createElement('div');
-    navDiv.classList.add('header-navs');
-
-    for (let i = 0; i < 3; i++){
-        const nav = document.createElement('span');
-        nav.classList.add('header-nav');
-
-        if (i === 0) {
-            nav.setAttribute('id', 'home');
-            nav.textContent = 'Home';
-        }
-        else if (i === 1) {
-            nav.setAttribute('id', 'menu');
-            nav.textContent = 'Menu';
-        }
-        else {
-            nav.setAttribute('id', 'contact');
-            nav.textContent = 'Contact';
-        }
-        navDiv.appendChild(nav);
-    }
-
-    headerDiv.appendChild(title);
-    headerDiv.appendChild(navDiv);
-    headerTag.appendChild(headerDiv)
-    body.appendChild(headerTag);
-}
-
-
 // Renders the top part of the home page when the page is loaded.
-const welcomePage = (headerTag) => {
+const welcomeSection = () => {
     const body = document.querySelector('body');
     const contentContainer = document.createElement('div');
     const aboutButton = document.createElement('button');
+    const headerTag = document.createElement('header');
+    const header = createHeader();
+
 
     contentContainer.setAttribute('id', 'content-container');
+    contentContainer.appendChild(headerTag);
 
     const welcome = renderContent('welcome', 'welcome-text', 'Welcome to Otit&#7885', 'span');
     const headlineDiv = renderContent('head', 'headline', 'Authentic Nigerian Cuisine', 'h1');
     aboutButton.setAttribute('id', 'about-button'); aboutButton.textContent = 'About Us';
 
-    const elementArray = [welcome, headlineDiv, aboutButton];
-    elementArray.forEach(element => contentContainer.appendChild(element));
-    headerTag.appendChild(contentContainer);
-    
-    body.appendChild(headerTag);
+    const elementArray = [header, welcome, headlineDiv, aboutButton];
+    elementArray.forEach(element => headerTag.appendChild(element));
 
+    body.appendChild(contentContainer);
 }
 
 const aboutSection = () => {
@@ -74,8 +39,11 @@ const aboutSection = () => {
     elementArray.forEach(element => aboutSection.appendChild(element));
 
     body.appendChild(aboutSection);
+}
 
-
+const createHomePage = () => {
+    welcomeSection();
+    aboutSection();
 }
  
-export { createHeader, welcomePage, aboutSection};    
+export { createHomePage }
